ae4ef91b7151b64bd63bc0e4af71bd83ace1d4df
MargoLeong <52899083+MargoLeong@users.noreply.github.com>
2020-05-10 07:45:44 -0700

    Update happy pwt java
    ?

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -14,11 +14,11 @@ String pet=JOptionPane.showInputDialog(null,"what pet do you want?");
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int care = JOptionPane.showOptionDialog(null, "Take Care Of Your",+pet + "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "make it exersize", "feed", "make it take a nap" }, null);
+			int care = JOptionPane.showOptionDialog(null, "Take Care Of Your "+ pet , pet, 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "make "+pet+"exersize", "feed", "make it take a nap" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-JOptionPane.
+JOptionPane.showMessageDialog(null, "you are going to make"+care);
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
